9c7c8c33000561a90878e94a38caf06cd5e762cc
Aaron <aaronko1005@gmail.com>
2024-03-18 14:20:33 +1300

    Finished all the dictionaries needed

---
+++ AS91896---Aaron-Ko.py
@@ -0,0 +1,62 @@
+tasks = {
+    'T1': {
+    'Title':'Design Homepage',
+    'Desciption':'Create a mockup of the homepage', 
+    'Assignee':'JSM',
+    'Priotiy': '3',
+    'status':'In Progress'
+    },
+
+    'T2': {
+    'Title':'Implement Login page',
+    'Desciption':'Create the Login page for the website', 
+    'Assignee':'JSM',
+    'Priotiy': '3',
+    'status':'Blocked'
+    }, 
+
+    'T3': {
+    'Title':'Fix navigation menu',
+    'Desciption':'Fix the navigation menu to be more user-friendly', 
+    'Assignee':'None',
+    'Priotiy': '1',
+    'status':'Not Started'
+    },
+
+    'T4': {
+    'Title':'Implement payment processing for the website',
+    'Desciption':'Implement payment processing for the website', 
+    'Assignee':'JLO',
+    'Priotiy': '2',
+    'status':'In Progress'
+    },
+
+    'T5': {
+    'Title':'Create an About Us page',
+    'Desciption':'Create a page with information about the company', 
+    'Assignee':'BDI',
+    'Priotiy': '1',
+    'status':'Blocked'
+    }
+}
+
+team_member = {
+    'JSM': {
+        'Name':'John Smith',
+        'Email':'John@techvision.com',
+        'Tasks Assigned': ['T1','T2']
+    },
+    'JLO': {
+        'Name':'Jane Love',
+        'Email':'Jane@techvision.com',
+        'Tasks Assigned': ['T4']
+    },
+    'BDI': {
+        'Name':'Bob Dillon',
+        'Email':'Bob@techvision.com',
+        'Tasks Assigned': ['T5']
+    },
+}
+
+
+
