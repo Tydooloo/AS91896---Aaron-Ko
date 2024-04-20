83d99c557caf7dbc7d58ecd5358db4cf75038e4a
Aaronzdfxfgn <aaron.ko@codebricks.co.nz>
2024-04-20 20:41:50 +1200

    Finished the generate report and the Update functions. BIG changes to the add task function so that title description functions can be reused. So Stupid that the stupid marking scedule says comments have to be wrutten as full sentences line length perfect and perfectly indented everytime like we still in the early 2000s. Marking scheme made by idiots

--- AS91896---Aaron-Ko.py
+++ AS91896---Aaron-Ko.py
@@ -56,98 +56,170 @@ team_member = {
 
 class task_management():
     def __init__(self, tasks):
+        # making tasks a global object within the class
         self.tasks = tasks
+        # output will allow the user to double check
+        self.output = {}
     
     def add_task(self):
-        # output will allow the user to double check
-        output = {}
-        # using and inline for loop, it looks through all IDs, extracts numeric value
+        # using  list comprehension, it looks through all IDs, extracts numeric value
         # and max() finds the highest one then +1 as it is sequentially unique
-        ID = max(int(key[1]) for key in self.tasks.keys()) + 1
-
-        # adding title
-        title = easygui.enterbox('Please enter your title')
-        # checking if the user hits 'x' or the cancel button
-        if title is None:
-            return
-        # making sure the dictionary is consistant
-        output[f'T{ID}'] = {'Title': title}
-
-        # adding desciption using the same method as the title
-        description = easygui.enterbox('Please enter your description')
-        if description is None:
-            return
-        output[f'T{ID}']['Description'] = description
-        
-        # Add assignee with enterbox
-        # While true so that the user can redo if they mess up
-        # while True:
-        #     assignee = easygui.enterbox('Please enter your assignees code')
-        #     assignee = assignee.upper()
-              # checking if assignee is found as one of the keys in the team_member dict  
-        #     if assignee in team_member.keys():
-        #         self.tasks[f'T{ID}']['Assignee'] = assignee
-        #         break
-        #     else:
-        #         easygui.msgbox('Sorry, that code does not exist')
-
-        # adding assignee using choicebox
-        choices = list(team_member.keys())
-        assignee = easygui.choicebox('Please choose your assignees code', choices = choices)
-        if assignee is None:
-            return
-        output[f'T{ID}']['Assignee'] = assignee
-
-        # adding priority rating using enterbox
-        # while True:
-              # using Value errors to make sure the code does not break even if the person types in a str or float
-        #     try:
-        #         priority = int(easygui.enterbox('Please enter your priority rating (1 - 3)'))
-                  # This is the Boundary          
-        #         if 1 <= priority <= 3:
-        #             # Convert to string for consistency in the dictionary
-        #             self.tasks[f'T{ID}']['Priority'] = str(priority)  
-        #             break
-        #         else:
-        #             easygui.msgbox('Sorry, that priority is invalid')
-        #     except ValueError:
-        #         easygui.msgbox('Sorry, invalid input. Please enter a number.')   
-
-        # adding priority rating using buttonbox
-        priority = easygui.buttonbox('Please choose your priority', choices = ['1','2','3'])
-        if priority is None:
-            return
-        output[f'T{ID}']['Priority'] = priority
-
-        # adding status
-        status = easygui.buttonbox('Please choose your Status', choices = ['Not Started','Blocked','In Progress'])
-        if status is None:
-            return
-        output[f'T{ID}']['Status'] = status
-
-        # making the verification page look good (could have used an inlined forloop up it woudl be too long)
+        self.ID = max(int(key[1]) for key in self.tasks.keys()) + 1
         output_verification = ''
-        for id, tasks in output.items():
+
+        # exits the function if 'x' or cancel is pressed at anytime
+        if not self.title():
+            return
+        if not self.description():
+            return
+        if not self.assignee():
+            return
+        if not self.priority():
+            return
+        if not self.status():
+            return
+        # calling each function inside of output 
+        self.output[f'T{self.ID}'] = {
+            'Title': self.title(),
+            'Description': self.description(),
+            'Assignee': self.assignee(),
+            'Priority': self.priority(),
+            'Status': self.status()
+        }
+
+        # making the verification page look good (could have used an inlined for loop up it would be too long)
+        for id, tasks in self.output.items():
             output_verification += f'\n{id}\n'
             for task_details, value in tasks.items():
                 output_verification += f'{task_details} : {value}\n'
+
         # asking for verification
         verification = easygui.buttonbox(f'Are you sure you would like to add this task?:\n{output_verification}', choices = ['Yes','No'])
 
         if verification == 'Yes':
             # to add the output to the tasks dictionary we use update() function
-            self.tasks.update(output)
+            self.tasks.update(self.output)
         else:
             return
+        # reset so that add task can be used again
+        self.output = {}
+
+    # make these in functions so that I dont have to reuse them for update function 
+    def title(self):
+        title = easygui.enterbox('Please enter your title')
+        # checking if the user hits 'x' or the cancel button
+        if title is None:
+            return
+        # making sure the dictionary is consistant
+        return title
+
+    def description(self):
+        # adding desciption using the same method as the title
+        description = easygui.enterbox('Please enter your description')
+        if description is None:
+            return
+        return description 
+
+    def assignee(self):
+        # adding assignee using choicebox
+        choices = list(team_member.keys())
+        assignee = easygui.choicebox('Please choose your assignees code', choices = choices)
+        if assignee is None:
+            return
+        return assignee
+    
+    def priority(self):
+        # adding priority rating using buttonbox
+        priority = easygui.buttonbox('Please choose your priority', choices = ['1','2','3'])
+        if priority is None:
+            return
+        return priority
+
+    def status(self):
+        # adding status
+        status = easygui.buttonbox('Please choose your Status', choices = ['Not Started','Blocked','In Progress', 'Completed'])
+        if status is None:
+            return
+        return status
 
     def update_task(self):
-        pass
+        # adding the ID AND title of task for easier navigation
+        choices = []
+        info = ''
+        for ID, details in tasks.items():
+            # adding ID of given iteration
+            info += ID
+            for key, value in details.items():
+                # this will add the actual title(value) into info
+                if key == 'Title':
+                    info += f' : {value}'
+            choices.append(info)
+            # resetting info to avoid tasks stacking up in one value of list
+            info = ''
+    
+        task_to_update = easygui.choicebox("What task would you like to Update", choices = choices)
+        # makes sure that the any NoneTypes can't cause errors
+        if task_to_update == None:
+            return
+        task_section_to_update = easygui.choicebox("What part of the task would you like to update?", choices = ['Title','Description','Assignee','Priority','Status'])
+        if task_section_to_update == None:
+            return
+        # get the task ID from task_to_update 
+        # split at ':' get the first value [0] in the list and then strip white space out after
+        task_id = task_to_update.split(':')[0].strip()
+
+
+        # calling all functions based of task_section_to_update
+        if task_section_to_update == 'Title':
+            title = self.title()
+            self.tasks[task_id]['Title'] = title
+
+        if task_section_to_update == 'Description':
+            description = self.description()
+            self.tasks[task_id]['Description'] = description
+
+        if task_section_to_update == 'Assignee':
+            assignee = self.assignee()
+            self.tasks[task_id]['Assignee'] = assignee
+
+        if task_section_to_update == 'Priority':
+            priority = self.priority()
+            self.tasks[task_id]['Priority'] = priority
+
+        if task_section_to_update == 'Status':
+            status = self.status()
+            # makes this task dissappear from the team members Tasks Assigned
+            if status == 'Completed':
+                # get the assignee code
+                assignee_code = self.tasks[task_id]['Assignee']
+                # remove the completed task from the assigned tasks list of the team member
+                # dictionary.get(key, default_value)x2 (nested) for loops take to much code
+                if task_id in team_member.get(assignee_code, {}).get('Tasks Assigned', []):
+                    team_member[assignee_code]['Tasks Assigned'].remove(task_id)
+                # Update the task status
+                self.tasks[task_id]['Status'] = status
+                
+
+        
+
+        
 
     def search(self):
         pass
 
+
     def generate_report(self):
-        pass
+        # initializing a counter 
+        status_counts = {'Completed': 0, 'In Progress': 0, 'Blocked': 0, 'Not Started':0}
+        # looking through all the task values
+        for task in self.tasks.values():
+            # adding counter to corresponding values in 'status'
+            status_counts[task['Status']] += 1
+            # creates a list (list comprehension) of status, count pairs and joins em as a str
+        report_text = '\n'.join(f"{status} Tasks: {count}" for status, count in status_counts.items())
+        easygui.msgbox(report_text)
+
+
 
     def print_all(self):
         output = ''
@@ -155,10 +227,15 @@ class task_management():
             output += f'\n{ID}\n'
             for attribute, value in task_details.items():
                 output += f'{attribute} : {value}\n'
+        output += '___________________________________________\n\nTeam Members\n'
+        for code, details in team_member.items():
+            output += f'\n{code}\n'
+            for information, info in details.items():
+                output += f'{information} : {info}\n'
         easygui.msgbox(output)
+
 
 
 # Start
 get = task_management(tasks)
-get.add_task()
 get.print_all()
